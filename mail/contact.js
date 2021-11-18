4b62784be6a96e32dae65f3f52911f490dd71819
Nicolas Chamorro <nichagiro@gmail.com>
2021-11-18 17:02:47 -0500

    seo +

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
+                url: "mail/contact.php",
                 type: "POST",
                 data: {
                     name: name,
@@ -29,7 +29,7 @@ $(function () {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
+                            .append("<strong>Su mensaje se ha enviado con exito!. </strong>");
                     $('#success > .alert-success')
                             .append('</div>');
                     $('#contactForm').trigger("reset");
--- mail/contact.php
+++ mail/contact.php
@@ -9,7 +9,7 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "admin@overweb.com.co"; // Change this email to your //
+$to = "info@agrometales.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "Has recibido un nuevo mensaje desde el sitio web.\n\n"."Aqui estan los detalles:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
@@ -17,4 +17,4 @@ $header .= "Reply-To: $email";
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
-?>
+
